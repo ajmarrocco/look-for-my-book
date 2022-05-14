7106e11f1b8d50c5730801df8028ace3f2637881
ajmarrocco <ajmarrocco@gmail.com>
2022-05-14 00:46:24 -0400

    add queries to typeDefs

--- server/schema/typeDef.js
+++ server/schema/typeDef.js
@@ -2,7 +2,16 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors:[], description: String!, title: String!, bookId: ID!, image: String!, link:! String): User
+    }
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
